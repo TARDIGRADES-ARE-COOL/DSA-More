33adc95389eedb688cd703f9522d96fdf1b0811f
Sarvesh Joaquim Gopu <129653763+TARDIGRADES-ARE-COOL@users.noreply.github.com>
2026-01-02 00:39:54 +0800

    Time: 1 ms (49.12%) | Memory: 17.3 MB (93.44%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,28 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s)<=1:
-            return False
         temp = []
-        for i in (s):
-            if i in ["(","[", "{"]:
-                temp.append(i)
-            # elif i in [")", "]", "}"] and temp[-1] in ["(","[", "{"]:
-            #     temp.pop()
-            else:
-                if not temp:          # <- important guard
-                    return False
-                elif (i == ")" and  temp[-1] == "("):
-                    temp.pop()
-                elif (i == "]" and  temp[-1] == "["):
-                    temp.pop()
-                elif (i == "}" and  temp[-1] == "{"):
-                    temp.pop()
-                else:
-                    return False
-
-
-
-        if len(temp) == 0:
+        if len(s) == 0:
             return True
-        else:
-            return False        
+        
+        for i in s:
+            if i in ["[", "(", "{"]:
+                temp.append(i)
+            elif i == "]" and temp and temp[-1] == "[":
+                temp.pop()
+            elif i == "}" and temp and temp[-1] == "{":
+                temp.pop()
+            elif i == ")" and temp and temp[-1] == "(":
+                temp.pop()
+            else:
+                return False
+        
+        return len(temp) == 0
