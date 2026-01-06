7aff33c6b1d80a706aba2cfbf5994558de8206cb
Sarvesh Joaquim Gopu <129653763+TARDIGRADES-ARE-COOL@users.noreply.github.com>
2026-01-07 01:06:00 +0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        steps = [0,1,2] #if n = 1 take 1 step , if n = 2 take 2 step
-        if n in steps:
-            return steps[n]
+        stored = [0,1,2]
 
-        if n is None:
-            return None
+        if n in stored:
+            return stored[n]
         
-        for index in range(3, n+1):
-            next_amount_steps = steps[index-1] + steps[index-2]
-            steps.append(next_amount_steps)
-        return steps[n]
+        if n is None:
+            return
+
+    
+
+
+        for i in range(3, n+1):
+            ways = stored[i-1] + stored[i-2]
+            stored.append(ways)
+
+        return stored[-1]
+        
