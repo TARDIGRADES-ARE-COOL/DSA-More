7944e53785f0430dbda146b30887ada64c2bbf28
Sarvesh Joaquim Gopu <129653763+TARDIGRADES-ARE-COOL@users.noreply.github.com>
2026-01-09 01:18:23 +0800

    Time: 88 ms (24.62%) | Memory: 25.4 MB (5.23%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,24 +1,36 @@
+# import heapq
+# from math import *
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+#         heap = []
+#         res = []
+        
+#         for x , y in points:
+#             distance = (x**2) + (y**2)
+#             heap.append([distance, [x, y]])
+
+#         heapq.heapify(heap)
+
+#         for i in range(k):
+#             distance, cord = heapq.heappop(heap)
+#             res.append(cord)
+
+#         return res
+
 import heapq
-from math import *
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        heap = []
-        res = []
-        
-        for x , y in points:
-            distance = (x**2) + (y**2)
-            heap.append([distance, [x, y]])
-
-        heapq.heapify(heap)
-
-        for i in range(k):
-            distance, cord = heapq.heappop(heap)
-            res.append(cord)
-
+        minheap=[]
+        for x,y in points:
+            dist= (x*x) + (y*y)
+            minheap.append([dist,x,y])
+        heapq.heapify(minheap)
+        res=[]
+        while k>0:
+            dist,x,y= heapq.heappop(minheap)
+            res.append([x,y])
+            k-=1
         return res
-
-
-
-
 
         
